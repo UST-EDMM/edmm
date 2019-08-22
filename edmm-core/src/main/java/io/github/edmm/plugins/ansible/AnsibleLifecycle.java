f9313f08108d392b7582da01b92ac47bdd7534f8
Vladimir Yussupov <v.yussupov@gmail.com>
2019-08-22 14:36:05 +0200

    Add structure traversal boilerplate code
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>

--- edmm-core/src/main/java/io/github/edmm/plugins/ansible/AnsibleLifecycle.java
+++ edmm-core/src/main/java/io/github/edmm/plugins/ansible/AnsibleLifecycle.java
@@ -1,49 +1,76 @@
 package io.github.edmm.plugins.ansible;
 
 import freemarker.template.Configuration;
-import freemarker.template.Template;
+import io.github.edmm.core.parser.Entity;
+import io.github.edmm.core.parser.EntityGraph;
 import io.github.edmm.core.plugin.AbstractLifecycle;
 import io.github.edmm.core.plugin.PluginFileAccess;
 import io.github.edmm.core.plugin.TemplateHelper;
 import io.github.edmm.core.transformation.TransformationContext;
+import io.github.edmm.model.DeploymentModel;
+import io.github.edmm.model.component.RootComponent;
+import io.github.edmm.model.relation.RootRelation;
+import org.jgrapht.alg.CycleDetector;
+import org.jgrapht.graph.DirectedMultigraph;
+import org.jgrapht.graph.EdgeReversedGraph;
+import org.jgrapht.traverse.TopologicalOrderIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
 public class AnsibleLifecycle extends AbstractLifecycle {
 
-    private static final Logger logger = LoggerFactory.getLogger(AnsibleLifecycle.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnsibleLifecycle.class);
 
     private final TransformationContext context;
+    private DeploymentModel model;
+    private Configuration cfg;
 
     public AnsibleLifecycle(TransformationContext context) {
         this.context = context;
+        this.model = context.getModel();
+        //this.cfg = TemplateHelper.fromClasspath(new ClassPathResource("plugins/ansible"));
     }
-
-    private Configuration cfg;
 
     @Override
     public void prepare() {
-        logger.info("Prepare transformation for Ansible...");
-        cfg = TemplateHelper.fromClasspath(new ClassPathResource("plugins/ansible"));
+        LOGGER.info("Prepare transformation for Ansible...");
     }
 
     @Override
     public void transform() {
-        logger.info("Begin transformation to Ansible...");
-        // TODO
-
+        LOGGER.info("Begin transformation to Ansible...");
         PluginFileAccess fileAccess = context.getFileAccess();
+
+        CycleDetector<RootComponent, RootRelation> cycleDetector = new CycleDetector<>(model.getTopology());
+        if (cycleDetector.detectCycles()) {
+            // TODO handle cycle in the topology notification
+            throw new RuntimeException("The given topology is acyclic");
+        } else {
+            // reverse the graph to find sources
+            EdgeReversedGraph<RootComponent, RootRelation> dependencyGraph = new EdgeReversedGraph<>(model.getTopology());
+            // apply topological sort
+            TopologicalOrderIterator<RootComponent, RootRelation> iterator = new TopologicalOrderIterator<>(dependencyGraph);
+            LOGGER.info("topological order");
+            while (iterator.hasNext()) {
+                LOGGER.info(iterator.next().getName());
+            }
+        }
+
         try {
             // TODO
         } catch (Exception e) {
             e.printStackTrace();
         }
-        logger.info("Transformation to Ansible successful");
+        LOGGER.info("Transformation to Ansible successful");
     }
 
     @Override
     public void cleanup() {
-        logger.info("Cleanup transformation leftovers...");
+        LOGGER.info("Cleanup transformation leftovers...");
     }
+
+
+
+
 }
--- edmm-core/src/test/java/io/github/edmm/plugins/ansible/AnsibleLifecycleTest.java
+++ edmm-core/src/test/java/io/github/edmm/plugins/ansible/AnsibleLifecycleTest.java
@@ -5,11 +5,14 @@ import java.nio.file.Files;
 
 import io.github.edmm.core.transformation.Transformation;
 import io.github.edmm.core.transformation.TransformationContext;
+import io.github.edmm.model.DeploymentModel;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AnsibleLifecycleTest {
 
@@ -17,7 +20,13 @@ public class AnsibleLifecycleTest {
 
     @Test
     public void run() throws Exception {
+        ClassPathResource resource = new ClassPathResource("templates/scenario_iaas.yml");
+        DeploymentModel model = DeploymentModel.of(resource.getFile());
         Transformation transformation = mock(Transformation.class);
+
+        when(transformation.getModel()).thenReturn(model);
+
+
         File rootDirectory = Files.createTempDirectory("ansible-").toFile();
         logger.info("Root directory is '{}'", rootDirectory);
         TransformationContext context = new TransformationContext(transformation, rootDirectory);
