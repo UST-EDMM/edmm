4838a1636c0411aeb2ceb2b479c029f7a62b2772
Vladimir Yussupov <v.yussupov@gmail.com>
2019-08-29 11:42:58 +0200

    Add initial chef repository generation
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>
    
    Former-commit-id: 187717281e907abe3ea1df3398fd07e7dcb0a410

---
+++ edmm-core/src/main/resources/plugins/chef/Policyfile.rb
@@ -0,0 +1,19 @@
+# Policyfile.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile.html
+
+# A name that describes what the system you're building with Chef does.
+name <#if policyfile.name??>'${policyfile.name}'<#else>'generated'</#if>
+
+# Where to find external cookbooks [current default = public Chef Supermarket]:
+default_source :supermarket
+default_source :chef_repo, '../cookbooks'
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list ${policyfile.cookbookString}
+
+# Specify a custom source for a single cookbook:
+<#list policyfile.cookbooks as cookbook>
+cookbook '${cookbook.name}}', path: '${cookbook.path}}'
+</#list>
