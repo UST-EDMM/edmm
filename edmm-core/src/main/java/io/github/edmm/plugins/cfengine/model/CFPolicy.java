9f6528b7fc6db23e971ec4fe5cfedeb6ae45c555
Vallasc <giacomovallorani.97@live.it>
2020-01-09 18:49:26 +0100

    Clean up

--- edmm-core/src/main/java/io/github/edmm/plugins/cfengine/model/CFPolicy.java
+++ edmm-core/src/main/java/io/github/edmm/plugins/cfengine/model/CFPolicy.java
@@ -2,8 +2,6 @@ package io.github.edmm.plugins.cfengine.model;
 
 import lombok.Builder;
 import lombok.Data;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -14,7 +12,6 @@ import java.util.Map;
  * Abstraction of a policy
  */
 public class CFPolicy {
-    private static final Logger logger = LoggerFactory.getLogger(CFPolicy.class);
     public final Map<String, String> modVars;
     public final Map<String, Map<String, String>> envVars;
     public final Map<String, String> classes;
