330adfe866c1fac2f7ca3f2d3ddf4c573019602a
Michael Wurster <miwurster@googlemail.com>
2020-04-06 07:51:55 +0200

    Add endpoint to retrieve list of plugins

--- edmm-web/src/main/java/io/github/edmm/web/controller/PluginController.java
+++ edmm-web/src/main/java/io/github/edmm/web/controller/PluginController.java
@@ -1,15 +1,17 @@
 package io.github.edmm.web.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import io.github.edmm.core.plugin.PluginService;
 import io.github.edmm.model.DeploymentModel;
 import io.github.edmm.model.PluginSupportResult;
+import io.github.edmm.web.model.PluginResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,16 +29,23 @@ public class PluginController {
         this.pluginService = pluginService;
     }
 
-    @Operation(summary = "Checks if a plugin supports the used components in an EDMM model.")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Returns a list of supported plugins.")
+    public List<PluginResult> getPlugins() {
+        return pluginService.getPlugins().stream()
+            .map(PluginResult::of).collect(Collectors.toList());
+    }
+
     @PostMapping(
         value = "/check-model-support",
         consumes = MediaType.TEXT_PLAIN_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<PluginSupportResult>> checkModelSupport(
+    @Operation(summary = "Checks if a plugin supports the used components in an EDMM model.")
+    public List<PluginSupportResult> checkModelSupport(
         @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "The plain EDMM YAML input")
         @RequestBody String yaml
     ) {
         DeploymentModel model = DeploymentModel.of(yaml);
-        return ResponseEntity.ok(this.pluginService.checkModelSupport(model));
+        return this.pluginService.checkModelSupport(model);
     }
 }
---
+++ edmm-web/src/main/java/io/github/edmm/web/model/PluginResult.java
@@ -0,0 +1,25 @@
+package io.github.edmm.web.model;
+
+import javax.validation.constraints.NotBlank;
+
+import io.github.edmm.core.plugin.Plugin;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PluginResult {
+
+    @NotBlank
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    public static PluginResult of(Plugin<?> plugin) {
+        return PluginResult.builder()
+            .id(plugin.getTargetTechnology().getId())
+            .name(plugin.getTargetTechnology().getName())
+            .build();
+    }
+}
