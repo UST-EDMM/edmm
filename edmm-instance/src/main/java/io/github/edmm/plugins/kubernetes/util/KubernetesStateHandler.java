4dc76eec590b07c3a40422a11f3b4f208909bbe3
Tobias Mathony <mathony.tobias@gmail.com>
2020-04-27 11:40:22 +0200

    clean code for state handling of kube

--- edmm-instance/src/main/java/io/github/edmm/plugins/kubernetes/util/KubernetesStateHandler.java
+++ edmm-instance/src/main/java/io/github/edmm/plugins/kubernetes/util/KubernetesStateHandler.java
@@ -2,26 +2,54 @@ package io.github.edmm.plugins.kubernetes.util;
 
 import io.github.edmm.model.edimm.InstanceState;
 import io.github.edmm.plugins.kubernetes.model.Status;
+import io.kubernetes.client.models.V1DeploymentCondition;
 import io.kubernetes.client.models.V1DeploymentStatus;
+import io.kubernetes.client.models.V1PodCondition;
 import io.kubernetes.client.models.V1PodStatus;
 
 public class KubernetesStateHandler {
 
     public static InstanceState.InstanceStateForDeploymentInstance getDeploymentInstanceState(V1DeploymentStatus status) {
 
-        if (Boolean.valueOf(status.getConditions().get(KubernetesConstants.LATEST_STATUS).getStatus())) {
-            return Status.KubernetesDeploymentStatus.valueOf(String.valueOf(status.getConditions().get(0).getType())).toEDiMMDeploymentInstanceState();
-        } else {
-            return InstanceState.InstanceStateForDeploymentInstance.ERROR;
+        if (isKubernetesDeploymentStatusSetToTrue(status)) {
+            return Status.KubernetesDeploymentStatus.valueOf(
+                String.valueOf(getLatestDeploymentCondition(status).getType()))
+                .toEDiMMDeploymentInstanceState();
         }
+        return InstanceState.InstanceStateForDeploymentInstance.ERROR;
     }
 
     protected static InstanceState.InstanceStateForComponentInstance getComponentInstanceState(V1PodStatus status) {
 
-        if (Boolean.valueOf(status.getConditions().get(KubernetesConstants.LATEST_STATUS).getStatus())) {
-            return Status.KubernetesPodStatus.valueOf(String.valueOf(status.getConditions().get(KubernetesConstants.LATEST_STATUS).getType())).toEDiMMComponentInstanceState();
-        } else {
-            return InstanceState.InstanceStateForComponentInstance.ERROR;
+        if (isKubernetesPodStatusSetToTrue(status)) {
+            return Status.KubernetesPodStatus.valueOf(
+                String.valueOf(getLatestPodCondition(status).getType()))
+                .toEDiMMComponentInstanceState();
         }
+        return InstanceState.InstanceStateForComponentInstance.ERROR;
+    }
+
+    private static boolean isKubernetesPodStatusSetToTrue(V1PodStatus status) {
+        return Boolean.valueOf(getLatestPodStatus(status));
+    }
+
+    private static boolean isKubernetesDeploymentStatusSetToTrue(V1DeploymentStatus status) {
+        return Boolean.valueOf(getLatestDeploymentStatus(status));
+    }
+
+    private static String getLatestPodStatus(V1PodStatus status) {
+        return getLatestPodCondition(status).getStatus();
+    }
+
+    private static String getLatestDeploymentStatus(V1DeploymentStatus status) {
+        return getLatestDeploymentCondition(status).getStatus();
+    }
+
+    private static V1PodCondition getLatestPodCondition(V1PodStatus status) {
+        return status.getConditions().get(KubernetesConstants.LATEST_STATUS);
+    }
+
+    private static V1DeploymentCondition getLatestDeploymentCondition(V1DeploymentStatus status) {
+        return status.getConditions().get(KubernetesConstants.LATEST_STATUS);
     }
 }
