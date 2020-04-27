da58ef0951fe711d2ca26b659f506af96f2ce7a0
Tobias Mathony <mathony.tobias@gmail.com>
2020-04-27 16:53:19 +0200

    add test for transformation

--- edmm-instance/src/test/java/io/github/edmm/core/TransformationTest.java
+++ edmm-instance/src/test/java/io/github/edmm/core/TransformationTest.java
@@ -1,4 +1,33 @@
 package io.github.edmm.core;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import io.github.edmm.core.parser.YamlParser;
+import io.github.edmm.core.parser.YamlTransformer;
+import io.github.edmm.model.edimm.DeploymentInstance;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 class TransformationTest {
+
+    @Test
+    void testCreateYamlForEDiMM() throws Exception {
+        ClassPathResource resource = new ClassPathResource("deployments/unit-tests/nginx-deployment_EDiMM.yaml");
+        DeploymentInstance deploymentInstance = new YamlParser().parseYamlAndTransformToDeploymentInstance(resource.getFile().getAbsolutePath());
+        YamlTransformer yamlTransformer = new YamlTransformer();
+        yamlTransformer.createYamlforEDiMM(deploymentInstance, "test-");
+        File file = new File(yamlTransformer.getFileOutputLocation());
+        String expectedString = FileUtils.readFileToString(resource.getFile(), StandardCharsets.UTF_8);
+        String actualString = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        assertNotNull(deploymentInstance);
+        assertTrue(file.exists());
+        assertTrue(file.length() > 0);
+        assertEquals(expectedString, actualString);
+    }
 }
--- edmm-instance/src/test/java/io/github/edmm/model/DeploymentInstanceTest.java
+++ edmm-instance/src/test/java/io/github/edmm/model/DeploymentInstanceTest.java
@@ -1,4 +1,11 @@
 package io.github.edmm.model;
 
+import org.junit.jupiter.api.Test;
+
 class DeploymentInstanceTest {
+
+    @Test
+    public void testDeploymentIntance() throws Exception {
+
+    }
 }
