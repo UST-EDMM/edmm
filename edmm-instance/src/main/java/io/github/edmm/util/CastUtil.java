51c81d406bfa21010d8f4d51acd92e44c908d454
Tobias Mathony <mathony.tobias@gmail.com>
2020-04-29 11:08:35 +0200

    rewrite cast util to avoid passing optional as arg

--- edmm-instance/src/main/java/io/github/edmm/util/CastUtil.java
+++ edmm-instance/src/main/java/io/github/edmm/util/CastUtil.java
@@ -8,31 +8,39 @@ import java.util.Optional;
 public class CastUtil {
 
     public static Map<String, Object> safelyCastToStringObjectMap(Object objectToCast) {
-        return castToStringObjectMapIfPresent(checkMapForCast(objectToCast));
+        Optional<Map> mapOptional = checkMapForCast(objectToCast);
+        return mapOptional.isPresent() ? castToStringObjectMap(mapOptional.get()) : Collections.emptyMap();
     }
 
     public static Map<String, String> safelyCastToStringStringMap(Object objectToCast) {
-        return castToStringStringMapIfPresent(checkMapForCast(objectToCast));
+        Optional<Map> mapOptional = checkMapForCast(objectToCast);
+        return mapOptional.isPresent() ? castToStringStringMap(mapOptional.get()) : Collections.emptyMap();
     }
 
     public static List<Object> safelyCastToObjectList(Object objectToCast) {
-        return castToObjectListIfPresent(checkListForCast(objectToCast));
+        Optional<List> listOptional = checkListForCast(objectToCast);
+        return listOptional.isPresent() ? castToObjectList(listOptional.get()) : Collections.emptyList();
     }
 
     public static List<String> safelyCastToStringList(Object objectToCast) {
-        return castToStringListIfPresent(checkListForCast(objectToCast));
+        Optional<List> listOptional = checkListForCast(objectToCast);
+        return listOptional.isPresent() ? castToStringList(listOptional.get()) : Collections.emptyList();
     }
 
-    private static List<Object> castToObjectListIfPresent(Optional<List> listOptional) {
-        return listOptional.isPresent() ? castToListOf(Object.class, listOptional.get()) : Collections.emptyList();
+    private static Map<String, Object> castToStringObjectMap(Map map) {
+        return castToMapOf(String.class, Object.class, map);
     }
 
-    private static List<String> castToStringListIfPresent(Optional<List> listOptional) {
-        return listOptional.isPresent() ? castToListOf(String.class, listOptional.get()) : Collections.emptyList();
+    private static Map<String, String> castToStringStringMap(Map map) {
+        return castToMapOf(String.class, String.class, map);
     }
 
-    private static Map<String, Object> castToStringObjectMapIfPresent(Optional<Map> mapOptional) {
-        return mapOptional.isPresent() ? castToMapOf(String.class, Object.class, mapOptional.get()) : Collections.emptyMap();
+    private static List<Object> castToObjectList(List list) {
+        return castToListOf(Object.class, list);
+    }
+
+    private static List<String> castToStringList(List list) {
+        return castToListOf(String.class, list);
     }
 
     private static Optional<Map> checkMapForCast(Object objectToCast) {
@@ -41,22 +49,10 @@ public class CastUtil {
             .map(Map.class::cast);
     }
 
-    private static Map<String, String> castToStringStringMapIfPresent(Optional<Map> mapOptional) {
-        return mapOptional.isPresent() ? castToMapOf(String.class, String.class, mapOptional.get()) : Collections.emptyMap();
-    }
-
     private static Optional<List> checkListForCast(Object objectToCast) {
         return Optional.ofNullable(objectToCast)
             .filter(List.class::isInstance)
             .map(List.class::cast);
-    }
-
-    private static <K> List<K> castToListOf(Class<K> kClass, List<?> listToCast) {
-        listToCast.forEach(entry -> checkCast(kClass, entry));
-
-        @SuppressWarnings("unchecked")
-        List<K> result = (List<K>) listToCast;
-        return result;
     }
 
     private static <K, V> Map<K, V> castToMapOf(Class<K> kClass, Class<V> vClass, Map<?, ?> map) {
@@ -68,6 +64,14 @@ public class CastUtil {
 
         @SuppressWarnings("unchecked")
         Map<K, V> result = (Map<K, V>) map;
+        return result;
+    }
+
+    private static <K> List<K> castToListOf(Class<K> kClass, List<?> listToCast) {
+        listToCast.forEach(entry -> checkCast(kClass, entry));
+
+        @SuppressWarnings("unchecked")
+        List<K> result = (List<K>) listToCast;
         return result;
     }
 
