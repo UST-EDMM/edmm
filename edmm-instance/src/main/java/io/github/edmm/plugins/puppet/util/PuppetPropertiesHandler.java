e9bf9e4ac7cec7010220fdcb64b33693f43c5c10
Tobias Mathony <mathony.tobias@gmail.com>
2020-06-05 14:10:00 +0200

    generate public key from private key for puppet master

--- edmm-instance/src/main/java/io/github/edmm/plugins/puppet/util/PuppetPropertiesHandler.java
+++ edmm-instance/src/main/java/io/github/edmm/plugins/puppet/util/PuppetPropertiesHandler.java
@@ -3,6 +3,14 @@ package io.github.edmm.plugins.puppet.util;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
@@ -10,6 +18,8 @@ import java.util.stream.Stream;
 import io.github.edmm.core.transformation.InstanceTransformationException;
 import io.github.edmm.model.edimm.InstanceProperty;
 import io.github.edmm.plugins.puppet.model.Fact;
+
+import org.apache.commons.codec.binary.Base64;
 
 public class PuppetPropertiesHandler {
     static List<InstanceProperty> getComponentInstanceProperties(List<Fact> facts) {
@@ -26,6 +36,7 @@ public class PuppetPropertiesHandler {
         instanceProperties.add(handleIp(ip));
         instanceProperties.add(handlePrivateKeyLocation(privateKeyLocation));
         instanceProperties.add(handleSSHPort(sshPort));
+        instanceProperties.add(handlePublicKey(privateKeyLocation));
 
         return instanceProperties;
     }
@@ -50,13 +61,41 @@ public class PuppetPropertiesHandler {
         return new InstanceProperty("SSHPort", sshPort.getClass().getSimpleName(), sshPort);
     }
 
+    private static InstanceProperty handlePublicKey(String privateKeyLocation) {
+        return new InstanceProperty("publicKey", String.class.getSimpleName(), generatePublicKeyFromPrivateKeyLocation(privateKeyLocation));
+    }
+
     private static String readPrivateKeyFileIntoString(String privateKeyLocation) {
         StringBuilder privateKey = new StringBuilder();
         try (Stream<String> stream = Files.lines(Paths.get(privateKeyLocation))) {
             stream.forEach(line -> privateKey.append(line).append("\n"));
         } catch (IOException e) {
-            throw new InstanceTransformationException("Failed to convert private key!");
+            throw new InstanceTransformationException("Failed to convert private key!", e.getCause());
         }
         return privateKey.toString();
+    }
+
+    private static String generatePublicKeyFromPrivateKeyLocation(String privateKeyLocation) {
+        String privateKeyString = readPrivateKeyFileIntoString(privateKeyLocation);
+        privateKeyString = privateKeyString.replace("-----BEGIN RSA PRIVATE KEY-----\n", "");
+        privateKeyString = privateKeyString.replace("-----END RSA PRIVATE KEY-----", "");
+        try {
+            byte[] decoded = Base64.decodeBase64(privateKeyString);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decoded);
+            java.security.Security.addProvider(
+                new org.bouncycastle.jce.provider.BouncyCastleProvider()
+            );
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+            RSAPrivateCrtKey privateCrtKey = (RSAPrivateCrtKey) privateKey;
+            RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(privateCrtKey.getModulus(), privateCrtKey.getPublicExponent());
+            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+            java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
+
+            return encoder.encodeToString(publicKey.getEncoded());
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            throw new InstanceTransformationException("Failed to convert public key", e.getCause());
+        }
     }
 }
