213d30e7d67ce73c50bc716cbaa1fccb48171854
Tobias Mathony <mathony.tobias@gmail.com>
2020-06-08 09:52:08 +0200

    correct command to copy keys to ag

--- edmm-instance/src/main/java/io/github/edmm/plugins/puppet/util/Commands.java
+++ edmm-instance/src/main/java/io/github/edmm/plugins/puppet/util/Commands.java
@@ -7,7 +7,7 @@ public class Commands {
     private static final String API = "http://localhost:8080/pdb/query/v4/";
     private static final String BASE_COMMAND = CURL_COMMAND + API;
     // TODO: make this more stable and less brittle e.g. by searching actively for where modules are stored, by checking environment, but for now its ok
-    public static final String COPY_PUBLIC_KEY = "cp ~/.ssh/puppet.pub /etc/puppetlabs/code/environments/production/modules/edimm_ssh/files/puppet.pub";
+    public static final String COPY_PUBLIC_KEY = "sudo cp ~/.ssh/puppet.pub /etc/puppetlabs/code/environments/production/modules/edimm_ssh/files/puppet.pub";
     private static final String SSH_KEY_GENERATION_PREFIX = "ssh-keygen -f ~/.ssh/";
     private static final String SSH_KEY_GENERATION_SUFFIX = " -t rsa -N ''";
     public static final String GET_MASTER = BASE_COMMAND + "producers";
