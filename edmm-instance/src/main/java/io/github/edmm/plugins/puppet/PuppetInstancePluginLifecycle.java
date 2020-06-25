42a7d8df0e6b46eec1d310c99f27bb195bad6e2a
Tobias Mathony <mathony.tobias@gmail.com>
2020-06-25 13:57:55 +0200

    start exporting servicetemplate to winery

--- edmm-instance/src/main/java/io/github/edmm/plugins/puppet/PuppetInstancePluginLifecycle.java
+++ edmm-instance/src/main/java/io/github/edmm/plugins/puppet/PuppetInstancePluginLifecycle.java
@@ -4,6 +4,7 @@ import io.github.edmm.core.plugin.AbstractLifecycleInstancePlugin;
 import io.github.edmm.core.transformation.InstanceTransformationContext;
 import io.github.edmm.core.transformation.TOSCATransformer;
 import io.github.edmm.core.yaml.YamlTransformer;
+import io.github.edmm.exporter.WineryExporter;
 import io.github.edmm.model.edimm.DeploymentInstance;
 import io.github.edmm.model.opentosca.ServiceTemplateInstance;
 import io.github.edmm.plugins.puppet.api.ApiInteractorImpl;
@@ -59,6 +60,7 @@ public class PuppetInstancePluginLifecycle extends AbstractLifecycleInstancePlug
     public void transformToTOSCA() {
         TOSCATransformer toscaTransformer = new TOSCATransformer();
         ServiceTemplateInstance serviceTemplateInstance = toscaTransformer.transformEDiMMToServiceTemplateInstance(this.deploymentInstance);
+        WineryExporter.exportServiceTemplateInstanceToWinery(serviceTemplateInstance);
         System.out.println("Transformed to OpenTOSCA Service Template Instance: " + serviceTemplateInstance.toString());
     }
 
