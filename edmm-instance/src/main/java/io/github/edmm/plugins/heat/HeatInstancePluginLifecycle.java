a80c5aef4cbaf93814b6607f968a3ae91843bf68
Tobias Mathony <mathony.tobias@gmail.com>
2020-06-29 10:18:51 +0200

    add export to winery for heat plugin

--- edmm-instance/src/main/java/io/github/edmm/plugins/heat/HeatInstancePluginLifecycle.java
+++ edmm-instance/src/main/java/io/github/edmm/plugins/heat/HeatInstancePluginLifecycle.java
@@ -8,6 +8,7 @@ import io.github.edmm.core.transformation.InstanceTransformationContext;
 import io.github.edmm.core.transformation.InstanceTransformationException;
 import io.github.edmm.core.transformation.TOSCATransformer;
 import io.github.edmm.core.yaml.YamlTransformer;
+import io.github.edmm.exporter.WineryExporter;
 import io.github.edmm.model.edimm.DeploymentInstance;
 import io.github.edmm.model.opentosca.ServiceTemplateInstance;
 import io.github.edmm.plugins.heat.api.ApiInteractorImpl;
@@ -80,6 +81,7 @@ public class HeatInstancePluginLifecycle extends AbstractLifecycleInstancePlugin
     public void transformToTOSCA() {
         TOSCATransformer toscaTransformer = new TOSCATransformer();
         ServiceTemplateInstance serviceTemplateInstance = toscaTransformer.transformEDiMMToServiceTemplateInstance(this.deploymentInstance);
+        WineryExporter.exportServiceTemplateInstanceToWinery(serviceTemplateInstance);
         System.out.println("Transformed to OpenTOSCA Service Template Instance: " + serviceTemplateInstance.toString());
     }
 
