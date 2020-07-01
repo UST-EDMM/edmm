a51e80d17e606a0735b108113394aedc5699d3bf
Tobias Mathony <mathony.tobias@gmail.com>
2020-07-01 20:12:34 +0200

    automatically apply FE via winery API, now only importing into container and register as instance is left

--- edmm-instance/src/main/java/io/github/edmm/exporter/WineryExporter.java
+++ edmm-instance/src/main/java/io/github/edmm/exporter/WineryExporter.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.List;
 
 import javax.xml.namespace.QName;
 
+import io.github.edmm.exporter.dto.EnrichmentDTO;
 import io.github.edmm.exporter.dto.ServiceTemplateCreationDTO;
 import io.github.edmm.exporter.dto.TopologyTemplateDTO;
 import io.github.edmm.model.opentosca.ServiceTemplateInstance;
@@ -17,6 +19,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ContentType;
@@ -25,17 +28,21 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 
 public class WineryExporter {
     private static String wineryEndpoint = "http://localhost:8080/winery/";
     private static String containerEndpoint = "http://localhost:1337/csars";
     private static String serviceTemplatesPath = "servicetemplates";
     private static String topologyTemplatePath = "topologytemplate";
+    private static String availableFeaturesPath = "availablefeatures";
     private static String csarDownloadPath = "?csar";
 
     public static void exportServiceTemplateInstanceToWinery(ServiceTemplateInstance serviceTemplateInstance, String outputPath) {
         createServiceTemplateInWinery(serviceTemplateInstance.getServiceTemplateId());
         createTopologyTemplateInWinery(serviceTemplateInstance);
+        // TODO
+        applyFeatures(getAvailableFeatures(serviceTemplateInstance.getServiceTemplateId()), serviceTemplateInstance.getServiceTemplateId());
         exportCSAR(serviceTemplateInstance.getServiceTemplateId(), outputPath);
         importCSARToContainer(outputPath);
     }
@@ -74,6 +81,36 @@ public class WineryExporter {
         }
     }
 
+    private static List<EnrichmentDTO> getAvailableFeatures(QName serviceTemplateId) {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet(wineryEndpoint + serviceTemplatesPath + doubleEncodeNamespace(serviceTemplateId) + topologyTemplatePath + "/" + availableFeaturesPath);
+        try {
+            get.setHeader("accept", "application/json");
+            HttpResponse response = httpClient.execute(get);
+
+            List<EnrichmentDTO> enrichmentDTOs = getJsonStringAsEnrichmentDTOs(EntityUtils.toString(response.getEntity()));
+            return enrichmentDTOs;
+        } catch (IOException e) {
+            System.out.println("Failed to get available features from Winery. Continue with creation of EDIMM YAML file.");
+        }
+        return null;
+    }
+
+    private static void applyFeatures(List<EnrichmentDTO> selectedFeatures, QName serviceTemplateId) {
+        if (selectedFeatures == null) {
+            return;
+        }
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPut put = new HttpPut(wineryEndpoint + serviceTemplatesPath + doubleEncodeNamespace(serviceTemplateId) + topologyTemplatePath + "/" + availableFeaturesPath);
+        try {
+            put.setHeader("content-type", "application/json");
+            put.setEntity(getObjectAsJson(selectedFeatures));
+            HttpResponse response = httpClient.execute(put);
+        } catch (IOException e) {
+            System.out.println("Failed to apply available features to Winery. Continue with creation of EDIMM YAML file.");
+        }
+    }
+
     private static void exportCSAR(QName serviceTemplateId, String outputPath) {
         try {
             FileUtils.copyURLToFile(new URL(wineryEndpoint + serviceTemplatesPath + doubleEncodeNamespace(serviceTemplateId) + csarDownloadPath), new File(outputPath));
@@ -96,9 +133,8 @@ public class WineryExporter {
             post.setEntity(entity);
 
             HttpResponse response = httpClient.execute(post);
-            System.out.println("OK");
         } catch (IOException e) {
-            System.out.println("Failed to create Service Template Instance in Winery. Continue with creation of EDIMM YAML file.");
+            System.out.println("Failed to import CSAR into Container. Continue with creation of EDIMM YAML file.");
         }
 
     }
@@ -106,6 +142,11 @@ public class WineryExporter {
     private static StringEntity getObjectAsJson(Object entity) throws UnsupportedEncodingException {
         Gson gson = new Gson();
         return new StringEntity(gson.toJson(entity));
+    }
+
+    private static List<EnrichmentDTO> getJsonStringAsEnrichmentDTOs(String jsonString) {
+        Gson gson = new Gson();
+        return gson.fromJson(jsonString, List.class);
     }
 
     private static String doubleEncodeNamespace(QName serviceTemplateId) {
---
+++ edmm-instance/src/main/java/io/github/edmm/exporter/dto/FeatureDTO.java
@@ -0,0 +1,8 @@
+package io.github.edmm.exporter.dto;
+
+import javax.xml.namespace.QName;
+
+public class FeatureDTO {
+    QName type;
+    String featureName;
+}
