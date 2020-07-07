11b695d041e1cac067cc44a0fb7dba23d9e3e6dc
Tobias Mathony <mathony.tobias@gmail.com>
2020-07-07 10:00:27 +0200

    add exception handling for puppet artifact download

--- edmm-instance/src/main/java/io/github/edmm/plugins/puppet/util/ModuleLoader.java
+++ edmm-instance/src/main/java/io/github/edmm/plugins/puppet/util/ModuleLoader.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import io.github.edmm.core.transformation.InstanceTransformationException;
+
 import org.apache.commons.io.FileUtils;
 
 class ModuleLoader {
@@ -22,7 +24,7 @@ class ModuleLoader {
                 new URL(baseURL + fileName),
                 new File(fileName));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new InstanceTransformationException("Failed to download artifacts required for Puppet Plugin.", e.getCause());
         }
     }
 }
